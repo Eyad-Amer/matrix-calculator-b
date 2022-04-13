a98c0b343874495c621084e11e103faf93e9c580
ubuntu <eyad.amer96@gmail.com>
2022-04-13 19:35:50 +0300

    done

--- sources/Matrix.cpp
+++ sources/Matrix.cpp
@@ -17,19 +17,40 @@ using namespace zich;
 
 namespace zich
 {
-    Matrix::Matrix(const vector<double> &mat, unsigned int r, unsigned int c)
+    // constructor
+    Matrix::Matrix(vector<double> mat, int row, int col)
     {
-        if (r <= 0 || c <= 0)
+        // the row and the column must be a positive number
+        if (row <= 0 || col <= 0)
         {
             throw invalid_argument("illegal inputs - the number of the row or columns are Negetave number!");
         }
 
-        this->rows = r;
-        this->cols = c;
+        // check if the size of the vector are the same of the matrix size
+        if (mat.size() != (row * col))
+        {
+            throw invalid_argument("illegal inputs - the size of the vector are the same of the matrix size!");
+        }
+
+        vector<double> v;
+        this->rows = row;
+        this->cols = col;
+
+        for (unsigned int i = 0; i < row * col; i++)
+        {
+            v.push_back(mat[i]);
+            if (col == v.size())
+            {
+                this->matrix.push_back(v);
+                v.resize(0);
+            }
+        }
     }
 
+    // copy constructor
     Matrix::Matrix(const Matrix &other)
     {
+        // the row and the column must be a positive number
         if (other.rows <= 0 || other.cols <= 0)
         {
             throw invalid_argument("illegal inputs - the number of the row or columns are Negetave number!");
@@ -37,12 +58,45 @@ namespace zich
 
         this->rows = other.rows;
         this->cols = other.cols;
+        this->matrix = other.matrix;
+    }
+
+    // get the rows
+    int Matrix::getMatrixRows() const
+    {
+        return this->rows;
+    }
+
+    // get the columns
+    int Matrix::getMatrixcols() const
+    {
+        return this->cols;
     }
 
     // check if the Matrices are the same size
-    bool checkSize(const Matrix &matrix1, const Matrix &matrix2)
+    void checkSize(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols)
+        {
+            throw invalid_argument("illegal");
+        }
+    }
+
+    // get the sum of the matrix members
+    double SumOfMatrix(const Matrix &matrix1)
+    {
+        double sum = 0;
+        Matrix matrix = matrix1;
+        int row = matrix.getMatrixRows();
+        int col = matrix.getMatrixcols();
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                sum += matrix.matrix[i][j];
+            }
+        }
+        return sum;
     }
 
     // Accounting operators: +, +=, +(onry), -, -=, -(onry)
@@ -50,134 +104,383 @@ namespace zich
     // plus
     Matrix Matrix::operator+(const Matrix &other)
     {
-        return *this;
+        checkSize(*this, other);
+
+        Matrix mat = other;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+        vector<double> vec;
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                vec.push_back(this->matrix[i][j] + other.matrix[i][j]);
+            }
+        }
+        Matrix plusMat{vec, row, col};
+
+        return plusMat;
     }
 
     // add
-    Matrix Matrix::operator+=(const Matrix &other)
+    Matrix &Matrix::operator+=(const Matrix &other)
     {
+        Matrix mat = *this;
+        checkSize(*this, other);
+
+        mat = mat + other;
+        this->matrix = mat.matrix;
+
         return *this;
     }
 
     // plus onry
     Matrix Matrix::operator+()
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+        vector<double> vec;
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                vec.push_back(this->matrix[i][j]);
+            }
+        }
+        Matrix onry{vec, row, col};
+
+        return onry;
     }
 
     // minus
     Matrix Matrix::operator-(const Matrix &other)
     {
-        return *this;
+        checkSize(*this, other);
+
+        Matrix mat = other;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+        vector<double> vec;
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                vec.push_back(this->matrix[i][j] - other.matrix[i][j]);
+            }
+        }
+        Matrix minusMat{vec, row, col};
+
+        return minusMat;
     }
 
     // reduction
-    Matrix Matrix::operator-=(const Matrix &other)
+    Matrix &Matrix::operator-=(const Matrix &other)
     {
+        Matrix mat = *this;
+        checkSize(*this, other);
+
+        mat = mat - other;
+        this->matrix = mat.matrix;
+
         return *this;
     }
 
     // minus onry
     Matrix Matrix::operator-()
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+        vector<double> vec;
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                vec.push_back(-this->matrix[i][j]);
+            }
+        }
+        Matrix onry{vec, row, col};
+
+        return onry;
+    }
+
+    // help function
+    bool comp(const Matrix &a, const Matrix &b, const string &s)
+    {
+        checkSize(a, b);
+        double mat1 = SumOfMatrix(a);
+        double mat2 = SumOfMatrix(b);
+
+        if (s == "<")
+        {
+            return (mat1 < mat2);
+        }
+
+        if (s == "<=")
+        {
+            return (mat1 <= mat2);
+        }
+
+        if (s == ">")
+        {
+            return (mat1 > mat2);
+        }
+
+        if (s == ">=")
+        {
+            return (mat1 >= mat2);
+        }
+
+        if (s == "!=")
+        {
+            return (mat1 != mat2);
+        }
+        return false;
     }
 
     // Comparison operators: <, <=, >, >=, =, !=
     bool operator<(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        string s = "<";
+        return (comp(matrix1, matrix2, s));
     }
 
     bool operator<=(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        string s = "<=";
+        return (comp(matrix1, matrix2, s));
     }
 
     bool operator>(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        string s = ">";
+        return (comp(matrix1, matrix2, s));
     }
 
     bool operator>=(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        string s = ">=";
+        return (comp(matrix1, matrix2, s));
     }
 
     bool operator==(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        checkSize(matrix1, matrix2);
+        Matrix mat = matrix1;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+        bool flag = true;
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                if (matrix1.matrix[i][j] != matrix2.matrix[i][j])
+                {
+                    flag = false;
+                }
+            }
+        }
+        return flag;
     }
 
     bool operator!=(const Matrix &matrix1, const Matrix &matrix2)
     {
-        return true;
+        string s = "!=";
+        return (comp(matrix1, matrix2, s));
     }
 
     // Increase or decrease by 1 all the members of the matrix befor the number
-    Matrix Matrix::operator++()
+    Matrix &Matrix::operator++()
     {
-        return *this;
+        for (unsigned int i = 0; i < rows; i++)
+        {
+            for (unsigned int j = 0; j < cols; j++)
+            {
+                this->matrix[i][j] += 1;
+            }
+        }
+        return (*this);
     }
 
-    Matrix Matrix::operator--()
+    Matrix &Matrix::operator--()
     {
-        return *this;
+        for (unsigned int i = 0; i < rows; i++)
+        {
+            for (unsigned int j = 0; j < cols; j++)
+            {
+                this->matrix[i][j] -= 1;
+            }
+        }
+        return (*this);
     }
 
     // Increase or decrease by 1 all the members of the matrix after the number
     Matrix Matrix::operator++(int num)
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                this->matrix[i][j] += 1;
+            }
+        }
+        return mat;
     }
 
     Matrix Matrix::operator--(int num)
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                this->matrix[i][j] -= 1;
+            }
+        }
+        return mat;
     }
 
     // Matrix doubling in scalar
     Matrix Matrix::operator*(double num)
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                mat.matrix[i][j] *= num;
+            }
+        }
+        return mat;
     }
 
-    Matrix Matrix::operator*=(double num)
+    Matrix &Matrix::operator*=(double num)
     {
-        return *this;
+        Matrix mat = *this;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
+
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                this->matrix[i][j] *= num;
+            }
+        }
+        return (*this);
     }
 
     // Matrix multiplication in the matrix
     Matrix operator*(double num, const Matrix &other)
     {
-        return other;
-    }
+        Matrix mat = other;
+        int row = mat.getMatrixRows();
+        int col = mat.getMatrixcols();
 
-    // Matrix multiplication in the matrix
-    Matrix operator*(const Matrix &other, double num)
-    {
-        return other;
+        for (unsigned int i = 0; i < row; i++)
+        {
+            for (unsigned int j = 0; j < col; j++)
+            {
+                mat.matrix[i][j] *= num;
+            }
+        }
+        return mat;
     }
 
     Matrix Matrix::operator*(const Matrix &other)
     {
-        return *this;
+        checkSize(*this, other);
+        Matrix mat = other;
+        int row1 = mat.getMatrixRows();
+        int col1 = mat.getMatrixcols();
+
+        int row2 = this->rows;
+        int col2 = this->cols;
+
+        vector<double> vec;
+
+        if (col2 != row1)
+        {
+            throw invalid_argument("error!");
+        }
+
+        for (unsigned int i = 0; i < row2; i++)
+        {
+            for (unsigned int j = 0; j < col1; j++)
+            {
+                vec.push_back(0);
+            }
+        }
+        unsigned int k = 0;
+        for (unsigned int i = 0; i < row2; i++)
+        {
+            for (unsigned int j = 0; j < col1; j++, k++)
+            {
+                for (unsigned int t = 0; t < col2; t++)
+                {
+                    vec[k] += this->matrix[i][t] * other.matrix[t][j];
+                }
+            }
+        }
+        Matrix multMat{vec, row2, col1};
+
+        return multMat;
     }
 
-    Matrix Matrix::operator*=(const Matrix &other)
+    Matrix &Matrix::operator*=(const Matrix &other)
     {
-        return *this;
+        checkSize(*this, other);
+
+        *this = ((*this) * other);
+        return (*this);
     }
 
     // input and output operator
     std::istream &operator>>(std::istream &input, Matrix &other)
     {
-        return input;
+        {
+            return input;
+        }
     }
 
     std::ostream &operator<<(std::ostream &output, const Matrix &other)
     {
-        return output;
+        {
+            for (unsigned int i = 0; i < other.rows; i++)
+            {
+                output << "[";
+                for (unsigned int j = 0; j < other.cols; j++)
+                {
+                    if (j < other.cols - 1)
+                    {
+                        output << other.matrix[i][j] << " ";
+                    }
+                    else
+                    {
+                        output << other.matrix[i][j];
+                    }
+                }
+                output << "]";
+                if (i < other.rows - 1)
+                {
+                    output << "\n";
+                }
+            }
+            return output;
+        }
     }
-
 }
--- sources/Matrix.hpp
+++ sources/Matrix.hpp
@@ -16,44 +16,40 @@ using namespace std;
 namespace zich{
     class Matrix{
         private:
-            unsigned int rows; // the rows of the matrix
-            unsigned int cols; // the columns of the matrix
+            int rows; // the rows of the matrix
+            int cols; // the columns of the matrix
             vector <vector <double>> matrix; // vector of vectors to create matrix
 
         public:
 
             // constructor
-            Matrix(const vector<double> &mat, unsigned int row, unsigned int col);
+            Matrix(vector<double> mat, int row, int col);
             Matrix(const Matrix &other);
 
             // get the rows
-            unsigned int getMatrixRows()
-            {
-                return this->rows;
-            }
+            int getMatrixRows() const;
 
             // get the columns
-            unsigned int getMatrixcols()
-            {
-                return this->cols;
-            }
+            int getMatrixcols() const;
 
             // get the matrix
-            vector <vector <double>> getMatrix()
-            {
-                return this->matrix;
-            }
+            //vector <vector <double>> getMatrix();
 
             // check if the Matrices are the same size
-            friend bool checkSize(const Matrix &matrix1, const Matrix &matrix2);
+            friend void checkSize(const Matrix &matrix1, const Matrix &matrix2);
+
+            // get the sum of the matrix members
+            friend double SumOfMatrix(const Matrix &matrix1);
+
+            // help function
+            friend bool comp(const Matrix &a, const Matrix &b, const string &s);
 
             // Accounting operators: +, +=, +(onry), -, -=, -(onry)
             Matrix operator+(const Matrix &other); // plus
-            Matrix operator+=(const Matrix &other); // add
+            Matrix &operator+=(const Matrix &other); // add
             Matrix operator+(); // plus onry
-
             Matrix operator-(const Matrix &other); // minus
-            Matrix operator-=(const Matrix &other); // reduction
+            Matrix &operator-=(const Matrix &other); // reduction
             Matrix operator-(); // minus onry
 
             // Comparison operators: <, <=, >, >=, =, !=
@@ -67,8 +63,8 @@ namespace zich{
             friend bool operator!=(const Matrix &matrix1, const Matrix &matrix2);
 
             // Increase or decrease by 1 all the members of the matrix befor the number
-            Matrix operator++();
-            Matrix operator--();
+            Matrix &operator++();
+            Matrix &operator--();
 
             // Increase or decrease by 1 all the members of the matrix after the number
             Matrix operator++(int num);
@@ -76,19 +72,16 @@ namespace zich{
 
             // Matrix doubling in scalar
             Matrix operator*(double num);
-            Matrix operator*=(double num);
+            Matrix &operator*=(double num);
 
             // Matrix multiplication in the matrix
             friend Matrix operator*(double num, const Matrix &other);
-            friend Matrix operator*(const Matrix &other, double num);
+            //friend Matrix operator*(const Matrix &other, double num);
             Matrix operator*(const Matrix &other);
-            Matrix operator*=(const Matrix &other);
+            Matrix &operator*=(const Matrix &other);
 
             // input and output operator
             friend std::istream &operator>>(std::istream &input, Matrix &other);
-            friend std::ostream &operator<<(std::ostream &output, const Matrix &other);
-             
-
+            friend std::ostream &operator<<(std::ostream &output, const Matrix &other);   
     };
 };
-
